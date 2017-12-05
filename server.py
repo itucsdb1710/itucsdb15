f9557c3f42cbdf992179209469de0053280c1971
Lina <Lina@192.168.1.12>
2017-12-05 07:08:05 +0300

    ...

--- server.py
+++ server.py
@@ -19,6 +19,15 @@ from restaurant import *
 
 lm = LoginManager()
 
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 @lm.user_loader
 def load_user(userName):
@@ -414,4 +423,11 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=5432 dbname='itucsdb'"""
+
     app.run(host='0.0.0.0', port=port, debug=debug)
