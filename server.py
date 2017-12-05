12b5fd7679633dcc9523cd709e23424455874779
Lina <Lina@192.168.1.12>
2017-12-05 06:56:44 +0300

    ..

--- server.py
+++ server.py
@@ -408,17 +408,9 @@ def initialize_database():
     return redirect(url_for('home_page'))
 
 if __name__ == '__main__':
-    app.secret_key = 'secret key'
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
-    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
-    if VCAP_SERVICES is not None:
-        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
-    else:
-        app.config['dsn'] = """user='vagrant' password='vagrant'
-                               host='localhost' port=5432 dbname='itucsdb'"""
-
     app.run(host='0.0.0.0', port=port, debug=debug)
